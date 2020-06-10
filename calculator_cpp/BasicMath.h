36d157690223570ae62e0a5486101afde64a2396
AndreiSerghie <32543048+AndreiSerghie@users.noreply.github.com>
2020-06-10 15:59:18 +0300

    The definition of the methods from the BasicMath class was moved outside the class

--- calculator_cpp/BasicMath.h
+++ calculator_cpp/BasicMath.h
@@ -8,20 +8,46 @@ namespace math
     class BasicMath
     {
     public:
-        typedef long double LD_t;
+        typedef long double LD_t; /* Use a short alias for the long double base type */
         LD_t FirstElement, SecondElement, Result;
         
-        /* Call ObjectConstructor function from print namespace upon object construction */
-        BasicMath() { print::ObjectConstructor(); }
+        BasicMath()
+        {
+            /* Call ObjectConstructor function from print namespace upon object construction */
+            print::ObjectConstructor();
+        }
 
-        LD_t Add(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 + refEl2; }
-        LD_t Sub(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 - refEl2; }
-        LD_t Mul(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 * refEl2; }
-        LD_t Div(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 / refEl2; }
+        LD_t Add(const LD_t &refEl1, const LD_t &refEl2);
+        LD_t Sub(const LD_t &refEl1, const LD_t &refEl2);
+        LD_t Mul(const LD_t &refEl1, const LD_t &refEl2);
+        LD_t Div(const LD_t &refEl1, const LD_t &refEl2);
 
-        /* Call ObjectDeconstructor function from print namespace upon object deconstruction */
-        ~BasicMath() { print::ObjectDeconstructor(); }
+        ~BasicMath()
+        {
+            /* Call ObjectDeconstructor function from print namespace upon object deconstruction */
+            print::ObjectDeconstructor();
+        }
     };
+    
+    LD_t BasicMath::Add(const LD_t &refEl1, const LD_t &refEl2)
+    {
+        return refEl1 + refEl2;
+    }
+    
+    LD_t BasicMath::Sub(const LD_t &refEl1, const LD_t &refEl2)
+    {
+        return refEl1 - refEl2;
+    }
+    
+    LD_t BasicMath::Mul(const LD_t &refEl1, const LD_t &refEl2)
+    {
+        return refEl1 * refEl2;
+    }
+    
+    LD_t BasicMath::Div(const LD_t &refEl1, const LD_t &refEl2)
+    {
+        return refEl1 / refEl2;
+    }
 }
 
 #endif // _BASIC_MATH_H_
