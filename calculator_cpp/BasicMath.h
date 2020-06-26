c5c280d4a43b0198a4bb41fc2470abbb9d01a9ab
AndreiSerghie <32543048+AndreiSerghie@users.noreply.github.com>
2020-06-26 08:38:05 +0300

    Use operator overloading techniques

--- calculator_cpp/BasicMath.h
+++ calculator_cpp/BasicMath.h
@@ -3,51 +3,39 @@
 
 #include "Print.h"
 
+#define USE_OPERATOR_OVERLOADING TRUE
+
 namespace math
 {
     class BasicMath
     {
     public:
-        typedef long double LD_t; /* Use a short alias for the long double base type */
+        typedef long double LD_t;
         LD_t FirstElement, SecondElement, Result;
-        
-        BasicMath()
-        {
-            /* Call ObjectConstructor function from print namespace upon object construction */
-            print::ObjectConstructor();
-        }
 
-        LD_t Add(const LD_t &refEl1, const LD_t &refEl2);
-        LD_t Sub(const LD_t &refEl1, const LD_t &refEl2);
-        LD_t Mul(const LD_t &refEl1, const LD_t &refEl2);
-        LD_t Div(const LD_t &refEl1, const LD_t &refEl2);
+        BasicMath() { print::ObjectConstructor(); }
 
-        ~BasicMath()
+        LD_t Add(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 + refEl2; }
+        LD_t Sub(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 - refEl2; }
+        LD_t Mul(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 * refEl2; }
+        LD_t Div(const LD_t &refEl1, const LD_t &refEl2) { return refEl1 / refEl2; }
+
+#if USE_OPERATOR_OVERLOADING == TRUE
+        /* Operator overloading */
+        LD_t operator+() { return FirstElement + SecondElement; }
+        LD_t operator-() { return FirstElement - SecondElement; }
+        LD_t operator*() { return FirstElement * SecondElement; }
+        LD_t operator/(LD_t temp)
         {
-            /* Call ObjectDeconstructor function from print namespace upon object deconstruction */
-            print::ObjectDeconstructor();
+            temp = FirstElement / SecondElement;
+            return temp;
         }
+#endif
+
+
+        ~BasicMath() { print::ObjectDeconstructor(); }
     };
-    
-    LD_t BasicMath::Add(const LD_t &refEl1, const LD_t &refEl2)
-    {
-        return refEl1 + refEl2;
-    }
-    
-    LD_t BasicMath::Sub(const LD_t &refEl1, const LD_t &refEl2)
-    {
-        return refEl1 - refEl2;
-    }
-    
-    LD_t BasicMath::Mul(const LD_t &refEl1, const LD_t &refEl2)
-    {
-        return refEl1 * refEl2;
-    }
-    
-    LD_t BasicMath::Div(const LD_t &refEl1, const LD_t &refEl2)
-    {
-        return refEl1 / refEl2;
-    }
 }
 
 #endif // _BASIC_MATH_H_
+
