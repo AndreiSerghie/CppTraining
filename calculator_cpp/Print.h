633ee01dff307687244828bb481e12b00ad47c8c
AndreiSerghie <32543048+AndreiSerghie@users.noreply.github.com>
2020-07-09 14:34:21 +0300

    Update Print.h

--- calculator_cpp/Print.h
+++ calculator_cpp/Print.h
@@ -1,14 +1,51 @@
 #ifndef _PRINT_H_
 #define _PRINT_H_
-
-using namespace std;
-
+/************************************************************************************/
+/*** ENUMERATIONS *******************************************************************/
+/************************************************************************************/
+enum ConstructorType
+{
+    SimpleContructor,
+    ParametrizedContructor,
+    CopyConstructor
+};
+/************************************************************************************/
+/*** IMPLEMENTATION *****************************************************************/
+/************************************************************************************/
 namespace print
 {
-    void ComputingStarted(void) { cout << "[INFO] Computing started!" << endl; }
-    void ComputingEnded(void) { cout << "[INFO] Computing ended!" << endl; }
-    void ObjectConstructor(void) { cout << "[INFO] Object is constructed!" << endl; }
-    void ObjectDeconstructor(void) { cout << "[INFO] Object is deconstructed!" << endl; }
+
+    void ComputingStarted(void)
+    {
+        std::cout << "[INFO] Computing started!" << std::endl;
+    }
+
+    void ComputingEnded(void)
+    {
+        std::cout << "[INFO] Computing ended!" << std::endl;
+    }
+
+    void ObjectConstructor(ConstructorType Type)
+    {
+        if (Type == SimpleContructor)
+        {
+            std::cout << "[INFO] Object is constructed! | Constructor type: Simple" << std::endl;
+        }
+        else if (Type == ParametrizedContructor)
+        {
+            std::cout << "[INFO] Object is constructed! | Constructor type: Parametrized" << std::endl;
+        }
+        else
+        {
+            std::cout << "[INFO] Object is constructed! | Constructor type: Copy constructor" << std::endl;
+        }
+    }
+
+    void ObjectDeconstructor(void)
+    {
+        std::cout << "[INFO] Object is destroyed!" << std::endl;
+    }
 }
 
 #endif // _PRINT_H_
+/************************************************************************************/
